9d8a5bb2bebba0c6ce0c0aacd2df1ec9fed22502
eddy mendoza <eddymy1304@gmail.com>
2024-07-07 23:24:01 -0500

    se subio la version de gradle a la 7.4.1, aun se puede subir la version de gradle.
    se cambio de grovvy a kotlin dsl
    
    se subio la version de sdk al 34
    
    se puede eliminar el archivo versions.gradle
    
    se implemento los catalogos de versiones, se pueden implementar los plugins de proyecto y app en los catalogs
    
    se pudo compilar de forma existosa la buildvariants rootDebug
    
    utilizar la jdl 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,14 +17,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-
-    ext.kotlin_version = '1.3.21'
-    apply from: 'versions.gradle'
-    addRepos(repositories)
+    extra["kotlin_version"] = "1.3.21"
+    //apply from: 'versions.gradle'
+    //addRepos(repositories)
     dependencies {
-        classpath deps.android_gradle_plugin
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10"
-        classpath "org.jetbrains.kotlin:kotlin-android-extensions:$kotlin_version"
+        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
+        classpath("org.jetbrains.kotlin:kotlin-android-extensions:1.3.21")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -32,17 +31,26 @@ buildscript {
     repositories {
         mavenCentral()
         google()
+        maven {
+            url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+        }
     }
 }
 
-allprojects {
+plugins {
+    id("com.android.application") version "7.4.2" apply false
+    id("com.android.library") version "7.4.2" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.3.21" apply false
+}
+
+/*allprojects {
     addRepos(repositories)
+}*/
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
-}
-
-repositories {
+/*repositories {
     google()
-}
+}*/
